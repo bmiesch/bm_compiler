a203d3e90fbcb06f6c08825c2718f9d7c8769e60
Brendan Miesch <mieschbr@umich.edu>
2024-06-11 14:03:52 -0700

    parser start

--- parser.cpp
+++ parser.cpp
@@ -1,12 +1,39 @@
-
+#include <vector>
+#include <memory>
 #include "lexer.cpp"
 
+// Forward declaration of the ASTNode class
+class ASTNode;
+
+// Define a type for a list of ASTNode shared pointers
+using ASTNodePtr = std::shared_ptr<ASTNode>;
+
+class ASTNode {
+public:
+    ASTNode(TokenType type) : type(type) {}
+    virtual ~ASTNode() = default;
+
+    TokenType getType() {return type;}
+
+    void addChild(ASTNodePtr child){
+        children.push_back(child);
+    }
+
+    const std::vector<ASTNodePtr>& getChildren() {
+        return children;
+    }
+
+private:
+    TokenType type;
+    std::vector<ASTNodePtr> children = 0;
+};
+
+class NumberNode : public ASTNode {
+public:
+    NumberNode() : ASTNode(TokenType::Number) {}
+};
 
 class Parser {
-private:
-    std::vector<Token> tokens;
-    int currrent = 0;
-
 public:
     Parser(const std::vector<Token>& tokens) : tokens(tokens) {}
 
@@ -14,4 +41,8 @@ public:
         // Start parsing and return the root of the AST
         return nullptr;
     }
+
+private:
+    std::vector<Token> tokens;
+    int current = 0;
 };
